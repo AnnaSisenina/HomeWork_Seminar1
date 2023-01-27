7df0544f626644600be57bc4b40f92aa91afb2ac
Anna Sisenina <yuta4ka71@gmail.com>
2023-01-28 02:23:40 +0300

    Task07 added

---
+++ Task07/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число ");
+int threedigitNumber=int.Parse(Console.ReadLine());
+if (Math.Abs(threedigitNumber)<100 || Math.Abs(threedigitNumber)>999)
+{
+    Console.WriteLine("Вы ввели не трехзначное число :(");
+    return;
+}
+int secondNumber = (threedigitNumber%100-threedigitNumber%10)/10;
+Console.WriteLine("Вторая цифра Вашего числа: "+secondNumber);

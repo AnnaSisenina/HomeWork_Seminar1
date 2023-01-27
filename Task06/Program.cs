416e2d25cdb7fcc699b33c57414bd690e14d3e89
Anna Sisenina <yuta4ka71@gmail.com>
2023-01-28 01:48:59 +0300

    Task06 added

---
+++ Task06/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число ");
+int threedigitNumber=int.Parse(Console.ReadLine());
+if (Math.Abs(threedigitNumber)<100 || Math.Abs(threedigitNumber)>999)
+{
+    Console.WriteLine("Вы ввели не трехзначное число :(");
+    return;
+}
+int remainder = threedigitNumber%10;
+Console.WriteLine("Последняя цифра Вашего числа: "+remainder);

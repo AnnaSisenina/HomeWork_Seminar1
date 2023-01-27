cd1d0d484a72c8f3a942c5fc88f3566e6610e415
Anna Sisenina <yuta4ka71@gmail.com>
2023-01-28 02:59:51 +0300

    Task10 added

---
+++ Task09/Program.cs
@@ -0,0 +1,12 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число ");
+int threedigitNumber=int.Parse(Console.ReadLine());
+if (Math.Abs(threedigitNumber)<100 || Math.Abs(threedigitNumber)>999)
+{
+    Console.WriteLine("Вы ввели не трехзначное число :(");
+    return;
+}
+int firstDigit = threedigitNumber/100;
+int thirdDigit = threedigitNumber%10;
+int newNumber = firstDigit*10+thirdDigit;
+Console.WriteLine("Число без второй цифры: "+newNumber);
---
+++ Task10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток
+int givenNumber = 3;
+Console.WriteLine("Введите число ");
+int number = int.Parse (Console.ReadLine());
+if (number%givenNumber==0)
+Console.WriteLine("Число "+number+" кратно "+givenNumber);
+else
+Console.WriteLine("Остаток от деления "+number+" на "+givenNumber+" равен "+number%givenNumber);

22ad039742df7dae133a4dbd8a11392598f9d545
Anna Sisenina <yuta4ka71@gmail.com>
2023-01-28 01:26:57 +0300

    Task05 added

---
+++ Task05/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать четные числа от 1 до N
+Console.WriteLine("Введите натуральное число ");
+int N = int.Parse(Console.ReadLine());
+if (N<1)
+{
+    Console.WriteLine("Вы ввели ненатуральное число :(");
+    return;
+}
+Console.WriteLine("Четные числа от 1 до "+N+":");
+for (int count =1;count<=N;count++)
+{
+    if (count%2==0)
+    Console.WriteLine(count);
+}

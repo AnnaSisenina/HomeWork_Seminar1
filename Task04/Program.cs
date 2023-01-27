92f9fe71f4f26e44ee4ffef266e0872c70fc97d2
Anna Sisenina <yuta4ka71@gmail.com>
2023-01-28 01:10:55 +0300

    Task04 added

---
+++ Task04/Program.cs
@@ -0,0 +1,7 @@
+﻿// Выяснить является ли число чётным
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+if (number%2==0)
+Console.WriteLine ("Ура! Введенное число четное!");
+else
+Console.WriteLine ("Введенное число нечетное :(");
